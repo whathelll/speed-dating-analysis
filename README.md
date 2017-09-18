0813034571beb95561ac217c2560c28b33071344
william.xu <william.xu@iag.com.au>
2017-09-18 23:51:47 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Speed dating analysis
+This repo is used for analysis of the data in the Kaggle Speed Dating Experiment data.
+https://www.kaggle.com/annavictoria/speed-dating-experiment/data
---
+++ project_common.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class ProjectCommon:
+    @staticmethod
+    def clean_data(df):
+        
+        print 'cleaning data: please add to this function'
+        df.iloc[1] = 1 
+        return df
+        
+
+        
